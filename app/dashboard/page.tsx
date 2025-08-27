b9d57a0c84448aa1f264fb00a62efe4458a33649
Usama Mansoor <usamamansoor79@gmail.com>
2025-08-27 10:22:46 +0500

    finished

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -51,7 +51,7 @@ async function GetPosts() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {data?.length > 0 &&
-        data.map((item: any) => <BlogPostCard key={item.id} data={item} />)}
+        data.map((item) => <BlogPostCard key={item.id} data={item} />)}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,8 @@ async function getPosts() {
       authorName: true,
       id: true,
       createdAt: true,
+      updatedAt: true,
+      authorId: true,
     },
   });
 
@@ -48,7 +50,7 @@ async function GetPosts() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {data?.length > 0 &&
-        data.map((item: any) => <BlogPostCard data={item} key={item.id} />)}
+        data.map((item) => <BlogPostCard data={item} key={item.id} />)}
     </div>
   );
 }
